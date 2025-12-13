ed3afb52a631ab30f30e8c9cea53418b9a3f1324
Sebastian Edholm <sebbe@sebb.io>
2025-12-13 02:07:51 +0100

    Initial commit config

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+set --universal --erase fish_greeting
+function fish_greeting; end
+funcsave fish_greeting
+
+set -gx EDITOR nvim
+set -gx BROWSER zen-browser
+set -gx RAD_HOME $HOME/.config/radicle
+set -gx PNPM_HOME "/home/sebbe/.local/share/pnpm"
+
+alias vim="nvim"
+alias rm="rm -iv"
+alias cp="cp -v"
+alias mv="mv -iv"
+alias lg="lazygit"
+
+bind alt-backspace backward-kill-word
+bind ctrl-backspace backward-kill-token
+
+abbr -a v nvim
+abbr -a k kubectl
+abbr -a incus sudo incus
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# opencode
+fish_add_path /home/sebbe/.opencode/bin
+# pnpmp
+fish_add_path /home/sebbe/.local/share/pnpm
+fish_add_path /opt/zen-browser-bin/
---
+++ link.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Inspired from: Dave Eddy <dave@daveeddy.com>
+#
+# Author: Sebastian Edholm <sebbe@sebb.io>
+# License: MIT
+
+
+defaults() {
+	echo defaults "$@"
+	command defaults "$@"
+}
+
+# verbose ln, because 'ln -v' is not portable
+symlink() {
+	printf '%55s -> %s\n' "${1/#$HOME/\~}" "${2/#$HOME/\~}"
+	ln -nsf "$@"
+}
+
+configs=(
+    fish/config.fish
+    fish/conf.d/dracula.fish
+    fish/conf.d/ssh-agent.fish
+    ghostty/config
+    git/.gitconfig
+    tmux/tmux.conf
+    yamlfmt/yamlfmt.yaml
+)
+
+for c in "${configs[@]}"; do
+    [[ -d ~/.config/$c && -L ~/.config/$c ]] && rm -r ~/.config/"$c"
+    mkdir -p ${c%/*}
+    symlink "$PWD/$c" ~/.config/"$f"
+done
