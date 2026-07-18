8955a1ae1183ed9cb1db470e1d42cc4bf01f87c2
Sebastian Edholm <sebbe@sebb.io>
2026-07-18 22:01:12 +0200

    update fontconfig

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,1 +1,6 @@
 o.exec_on_start("xfce-polkit")
+
+o.exec_on_start("xdg-settings set default-web-browser app.zen_browser.zen.desktop")
+o.exec_on_start("xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/http")
+o.exec_on_start("xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/https")
+o.exec_on_start("xdg-mime default app.zen_browser.zen.desktop text/html")
