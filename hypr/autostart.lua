2dc83d4766893006d8314c7953f79c667132d538
Sebastian Edholm <sebbe@sebb.io>
2026-07-18 23:32:22 +0200

    darkmode enable

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,5 +1,7 @@
 o.exec_on_start("xfce-polkit")
 
+o.exec_on_start("gsettings set org.gnome.desktop.interface color-scheme prefer-dark")
+
 o.exec_on_start("xdg-settings set default-web-browser app.zen_browser.zen.desktop")
 o.exec_on_start("xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/http")
 o.exec_on_start("xdg-mime default app.zen_browser.zen.desktop x-scheme-handler/https")
