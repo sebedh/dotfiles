1b4c8154da596ec2e3491d4c2643f38b6b6cfe8a
Sebastian Edholm <sebbe@sebb.io>
2026-07-18 23:01:46 +0200

    add input speed

--- hypr/input.lua
+++ hypr/input.lua
@@ -1,12 +1,13 @@
 hl.config({
   input = {
     kb_layout = "us,se",
-    kb_options = "grp:win_space_toggle,caps:escape,grp:shift_caps_toggle",
+    kb_options = "grp:lctrl_lwin_toggle,caps:escape,grp:shift_caps_toggle",
 
     repeat_rate = 50,
     repeat_delay = 300,
 
     numlock_by_default = true,
+    sensitivity = 0,
 
     touchpad = {
       scroll_factor = 0.4,
